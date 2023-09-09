3d7cd95fcc33a06c798f7beb546830bf117b587a
Nirob Ahmed <19-40925-2@student.aiub.edu>
2023-09-09 10:52:11 +0600

    v1.0

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.get('/', (req, res) => {
 async function main() {
     try {
         this.app = app;
-        
         router();
     } catch (err) {
         console.log(err);
@@ -33,3 +32,41 @@ mongoose.
     }).catch((error) => {
         console.log(error)
     })
+
+
+
+
+
+// const express = require('express');
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const app = express();
+// const router = require('./routeManager');
+
+// dotenv.config();
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome');
+// });
+
+// async function startServer() {
+//     try {
+//         app.app = app; 
+//         router();
+        
+//         await mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         });
+        
+//         app.listen(5000, () => {
+//             console.log('Server is listening on port 5000');
+//         });
+        
+//         console.log('Connected to the database');
+//     } catch (error) {
+//         console.error('An error occurred:', error);
+//     }
+// }
+
+// startServer();
